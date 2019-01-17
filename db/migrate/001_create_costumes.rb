700f51874301c91366b60e5d1a574bc44c624d46
github username <github email address>
2019-01-17 15:55:18 -0500

    Done.

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -15,9 +15,7 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
 			t.float :price
 			t.string :size
 			t.string :image_url
-		    t.datetime :created_at 
-		    t.datetime :updated_at
-		    t.timestamps null:false
+		    t.timestamps 
 		end
 	end
 end
